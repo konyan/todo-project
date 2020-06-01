616869408cb86326991d023bdca03b8ad275b295
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-06-01 23:07:46 +0200

    created the add todo function

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,45 @@
-console.log("HELLO WORLD");
+function Todo(title, description, dueDate) {
+  this.title = title;
+  this.description = description;
+  this.dueDate = dueDate;
+  //   this.priority = priority;
+}
+
+function UI() {}
+
+UI.prototype.addTodoList = function (todo) {
+  const todos = document.getElementById("todos");
+  const list = document.createElement("li");
+  list.innerHTML = `
+  <div class="form-check"> 
+  <label class="form-check-label"> 
+  <input class="checkbox" type="checkbox"> 
+    ${todo.title} | ${todo.description} | ${todo.dueDate}
+  <i class="input-helper"></i>
+  </label>
+   </div>
+  
+ <a href="#" data-id="">
+ <i class="fa fa-pencil"></i>
+</a>
+<i class="remove mdi mdi-close-circle-outline"></i>
+
+   `;
+  todos.appendChild(list);
+};
+
+document.getElementById("addTodos").addEventListener("submit", function (e) {
+  const title = document.getElementById("title").value,
+    description = document.getElementById("description").value,
+    dueDate = document.getElementById("dueDate").value;
+
+  const todo = new Todo(title, description, dueDate);
+
+  ui = new UI();
+
+  ui.addTodoList(todo);
+
+  e.preventDefault();
+});
+
+// Enable Edit State
